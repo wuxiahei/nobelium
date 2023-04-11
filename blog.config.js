8288349fd0e54404307fd37fe5585e143d95e7cb
wuxiahei <62011799+wuxiahei@users.noreply.github.com>
2023-04-12 00:02:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'flyone',
   email: 'wuxiahei@qq.com',
   link: 'https://nobelium.vercel.app',
-  description: 'awesome website.',
+  description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
